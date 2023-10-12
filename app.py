7a77c1655b401b1e347e006504ceb8fe3022aa4f
Ivan <zadorohhnyiv@gmail.com>
2023-10-12 23:44:58 +0300

    finished

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta
+import random
+import uuid
+
+app = Flask(__name__)
+
+users_data = {
+    "total": 20,
+    "data": []
+}
+
+for i in range(20):
+    user = {
+        "userId": str(uuid.uuid4()),
+        "nickname": f"User_{i}",
+        "firstName": f"FirstName_{i}",
+        "lastName": f"LastName_{i}",
+        "registrationDate": (datetime.now() - timedelta(days=random.randint(0, 365))).isoformat(),
+        "lastSeenDate": datetime.now().isoformat() if random.choice([True, False]) else None,
+        "isOnline": random.choice([True, False]),
+        "onlineTimePerWeek": random.randint(1000, 100000)
+    }
+    users_data['data'].append(user)
+
+@app.route('/api/stats/user/total', methods=['GET'])
+def get_total_time_online():
+    user_id = request.args.get('userId')
+    user = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    days_since_registration = (datetime.now() - datetime.fromisoformat(user["registrationDate"])).days
+    total_time = user["onlineTimePerWeek"] * days_since_registration / 7
+
+    return jsonify({"totalTime": int(total_time)})
+
+
+@app.route('/api/stats/user/average', methods=['GET'])
+def get_average_time_online():
+    user_id = request.args.get('userId')
+    user = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    days_since_registration = (datetime.now() - datetime.fromisoformat(user["registrationDate"])).days
+    total_time = user["onlineTimePerWeek"] * days_since_registration / 7
+
+    daily_average = total_time / days_since_registration
+    weekly_average = daily_average * 7
+
+    return jsonify({
+        "dailyAverage": int(daily_average),
+        "weeklyAverage": int(weekly_average)
+    })
+
+@app.route('/api/user/forget', methods=['POST'])
+def forget_user():
+    user_id = request.args.get('userId')
+    user = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    users_data['data'].remove(user)
+
+    return jsonify({"userId": user_id})
+
+
+@app.route('/api/users/ids', methods=['GET'])
+def get_user_ids():
+    user_ids = [user['userId'] for user in users_data['data']]
+    return jsonify(user_ids)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app_test.py
@@ -0,0 +1,109 @@
+from flask_testing import TestCase
+from selenium import webdriver
+import unittest
+from app import app, users_data
+import json
+from selenium.webdriver.chrome.options import Options
+
+
+# Unit Tests
+class FlaskTestCase(TestCase):
+
+    def create_app(self):
+        app.config['TESTING'] = True
+        return app
+
+    def test_total_time_valid_user(self):
+        user_id = users_data['data'][0]['userId']
+        response = self.client.get(f'/api/stats/user/total?userId={user_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('totalTime', response.json)
+
+    def test_total_time_invalid_user(self):
+        response = self.client.get('/api/stats/user/total?userId=nonexistent-id')
+        self.assertEqual(response.status_code, 404)
+
+    def test_average_time_valid_user(self):
+        user_id = users_data['data'][0]['userId']
+        response = self.client.get(f'/api/stats/user/average?userId={user_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('dailyAverage', response.json)
+        self.assertIn('weeklyAverage', response.json)
+
+    def test_forget_valid_user(self):
+        user_id = users_data['data'][0]['userId']
+        response = self.client.post(f'/api/user/forget?userId={user_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(user_id, [user['userId'] for user in users_data['data']])
+
+    def test_forget_already_deleted_user(self):
+        user_id = "some_deleted_id"
+        response = self.client.post(f'/api/user/forget?userId={user_id}')
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_user_ids(self):
+        response = self.client.get('/api/users/ids')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(isinstance(response.json, list))
+        for user_id in response.json:
+            self.assertTrue(any(user['userId'] == user_id for user in users_data['data']))
+
+
+# E2E Tests
+class FlaskE2ETestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        chrome_options = Options()
+        cls.driver = webdriver.Chrome(options=chrome_options)
+        cls.driver.implicitly_wait(10)
+
+    def test_total_time_e2e(self):
+        self.driver.get('http://127.0.0.1:5000/api/stats/user/total?userId=044fea9b-822a-4f44-832b-1808719277b2')
+
+        body_text = self.driver.find_element_by_tag_name('body').text
+
+        response_data = json.loads(body_text)
+
+        self.assertIn('totalTime', response_data)
+        self.assertIsInstance(response_data['totalTime'], int)
+        self.assertGreaterEqual(response_data['totalTime'], 0)
+
+    def test_average_time_e2e(self):
+        self.driver.get('http://127.0.0.1:5000/api/stats/user/average?userId=044fea9b-822a-4f44-832b-1808719277b2')
+
+        body_text = self.driver.find_element_by_tag_name('body').text
+
+        response_data = json.loads(body_text)
+
+        self.assertIn('dailyAverage', response_data)
+        self.assertIn('weeklyAverage', response_data)
+
+        self.assertIsInstance(response_data['dailyAverage'], int)
+        self.assertGreaterEqual(response_data['dailyAverage'], 0)
+
+        self.assertIsInstance(response_data['weeklyAverage'], int)
+        self.assertGreaterEqual(response_data['weeklyAverage'], 0)
+
+    def test_forget_user_e2e(self):
+        self.driver.get('http://127.0.0.1:5000/api/users/ids')
+        initial_user_ids_json = self.driver.find_element_by_tag_name('body').text
+        initial_user_ids = json.loads(initial_user_ids_json)
+
+        user_id_to_forget = initial_user_ids[0]
+        self.driver.get(f'http://127.0.0.1:5000/api/user/forget?userId={user_id_to_forget}')
+        response_body = self.driver.find_element_by_tag_name('body').text
+        self.assertIn(user_id_to_forget, response_body)
+
+        self.driver.get('http://127.0.0.1:5000/api/users/ids')
+        updated_user_ids_json = self.driver.find_element_by_tag_name('body').text
+        updated_user_ids = json.loads(updated_user_ids_json)
+        self.assertNotIn(user_id_to_forget, updated_user_ids)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
