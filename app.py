6e5c7bee36e97e517f38d6f6dfa062a8eff1411a
Ivan <zadorohhnyiv@gmail.com>
2023-11-09 23:51:13 +0200

    Reformat code with Black

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import uuid
 
 app = Flask(__name__)
 
-users_data = {
-    "total": 20,
-    "data": []
-}
+users_data = {"total": 20, "data": []}
 
 for i in range(20):
     user = {
@@ -16,65 +13,72 @@ for i in range(20):
         "nickname": f"User_{i}",
         "firstName": f"FirstName_{i}",
         "lastName": f"LastName_{i}",
-        "registrationDate": (datetime.now() - timedelta(days=random.randint(0, 365))).isoformat(),
-        "lastSeenDate": datetime.now().isoformat() if random.choice([True, False]) else None,
+        "registrationDate": (
+            datetime.now() - timedelta(days=random.randint(0, 365))
+        ).isoformat(),
+        "lastSeenDate": datetime.now().isoformat()
+        if random.choice([True, False])
+        else None,
         "isOnline": random.choice([True, False]),
-        "onlineTimePerWeek": random.randint(1000, 100000)
+        "onlineTimePerWeek": random.randint(1000, 100000),
     }
-    users_data['data'].append(user)
+    users_data["data"].append(user)
 
 
-@app.route('/api/stats/user/total', methods=['GET'])
+@app.route("/api/stats/user/total", methods=["GET"])
 def get_total_time_online():
-    user_id = request.args.get('userId')
-    user1 = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+    user_id = request.args.get("userId")
+    user1 = next((u for u in users_data["data"] if u["userId"] == user_id), None)
     if not user1:
         return jsonify({"error": "User not found"}), 404
 
-    days_since_registration = (datetime.now() - datetime.fromisoformat(user1["registrationDate"])).days
+    days_since_registration = (
+        datetime.now() - datetime.fromisoformat(user1["registrationDate"])
+    ).days
     total_time = user1["onlineTimePerWeek"] * days_since_registration / 7
 
     return jsonify({"totalTime": int(total_time)})
 
 
-@app.route('/api/stats/user/average', methods=['GET'])
+@app.route("/api/stats/user/average", methods=["GET"])
 def get_average_time_online():
-    user_id = request.args.get('userId')
-    user2 = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+    user_id = request.args.get("userId")
+    user2 = next((u for u in users_data["data"] if u["userId"] == user_id), None)
 
     if not user2:
         return jsonify({"error": "User not found"}), 404
 
-    days_since_registration = (datetime.now() - datetime.fromisoformat(user2["registrationDate"])).days
+    days_since_registration = (
+        datetime.now() - datetime.fromisoformat(user2["registrationDate"])
+    ).days
     total_time = user2["onlineTimePerWeek"] * days_since_registration / 7
 
     daily_average = total_time / days_since_registration
     weekly_average = daily_average * 7
 
-    return jsonify({
-        "dailyAverage": int(daily_average),
-        "weeklyAverage": int(weekly_average)
-    })
+    return jsonify(
+        {"dailyAverage": int(daily_average), "weeklyAverage": int(weekly_average)}
+    )
 
 
-@app.route('/api/user/forget', methods=['POST'])
+@app.route("/api/user/forget", methods=["POST"])
 def forget_user():
-    user_id = request.args.get('userId')
-    user3 = next((u for u in users_data['data'] if u['userId'] == user_id), None)
+    user_id = request.args.get("userId")
+    user3 = next((u for u in users_data["data"] if u["userId"] == user_id), None)
 
     if not user3:
         return jsonify({"error": "User not found"}), 404
 
-    users_data['data'].remove(user3)
+    users_data["data"].remove(user3)
 
     return jsonify({"userId": user_id})
 
 
-@app.route('/api/users/ids', methods=['GET'])
+@app.route("/api/users/ids", methods=["GET"])
 def get_user_ids():
-    user_ids = [user4['userId'] for user4 in users_data['data']]
+    user_ids = [user4["userId"] for user4 in users_data["data"]]
     return jsonify(user_ids)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
